8098aaffe06d3d73202f4d75fde3e98881943faf
Apuroopa Gottapu <agottapu@hm.local>
2018-06-28 10:43:55 -0400

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -18,9 +18,9 @@ const store=configureStore();
 
 
 
-store.dispatch(addExpense({description:'Water bill',amount:4500}));
-store.dispatch(addExpense({description:'Gas bill',createdAt:1000}));
-store.dispatch(addExpense({description:'Rent ',amount:109500}));
+// store.dispatch(addExpense({description:'Water bill',amount:4500}));
+// store.dispatch(addExpense({description:'Gas bill',createdAt:1000}));
+// store.dispatch(addExpense({description:'Rent ',amount:109500}));
 
 
 
@@ -29,11 +29,11 @@ store.dispatch(addExpense({description:'Rent ',amount:109500}));
 
 //setTimeout(()=>{ store.dispatch(setTextFilter('bill'));},3000)
 
-store.subscribe(()=>{
-    const state=store.getState();
-    const visibleExpenses=getVisibleExpenses(state.expenses,state.filters)
-    console.log(visibleExpenses);
-});
+// store.subscribe(()=>{
+//     const state=store.getState();
+//     const visibleExpenses=getVisibleExpenses(state.expenses,state.filters)
+//     console.log(visibleExpenses);
+// });
 
 const jsx=(
     <Provider store={store}>
