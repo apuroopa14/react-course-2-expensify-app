511e4ea4d2b9d781b775332f9296111ce4f82d38
Apuroopa Gottapu <agottapu@hm.local>
2018-07-05 14:42:59 -0400

    add private routes

--- src/app.js
+++ src/app.js
@@ -1,61 +1,44 @@
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import AppRouter from './routers/AppRouter';
-import {startSetExpenses} from './actions/expenses';
-import {setTextFilter} from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+import { Provider } from 'react-redux';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
-const store=configureStore();
-
-
-
-
-
-// store.dispatch(addExpense({description:'Water bill',amount:4500}));
-// store.dispatch(addExpense({description:'Gas bill',createdAt:1000}));
-// store.dispatch(addExpense({description:'Rent ',amount:109500}));
-
-
-
-//store.dispatch(setTextFilter('water'));
-
-
-//setTimeout(()=>{ store.dispatch(setTextFilter('bill'));},3000)
-
-// store.subscribe(()=>{
-//     const state=store.getState();
-//     const visibleExpenses=getVisibleExpenses(state.expenses,state.filters)
-//     console.log(visibleExpenses);
-// });
-
-const jsx=(
-    <Provider store={store}>
-
+const store = configureStore();
+const jsx = (
+  <Provider store={store}>
     <AppRouter />
-     </Provider> 
+  </Provider>
 );
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+};
 
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-
-///******************RENDERING**************************** */
-const appRoot=document.getElementById('app');
-
-
-ReactDOM.render(<p>Loading...</p>,appRoot);
-
-store.dispatch(startSetExpenses()).then(()=>{
-    ReactDOM.render(jsx,appRoot);
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+  } else {
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
+  }
 });
-
-
-
-
-
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,35 +1,29 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom';
-import AddExpensePage from '../components/AddExpensePage.js';
-import EditExpensePage from '../components/EditExpensePage.js';
-import ExpenseDashboardPage from '../components/ExpenseDashboardPage.js';
-import HelpPage from '../components/HelpPage.js';
-import NotFoundPage from '../components/NotFoundPage.js';
-import Header from '../components/Header.js';
+import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
+import LoginPage from '../components/LoginPage';
+import PrivateRoute from './PrivateRoute';
 
+export const history = createHistory();
 
-import {BrowserRouter,Route,Switch,Link,NavLink} from 'react-router-dom';
+const AppRouter = () => (
+  <Router history={history}>
+    <div>
+      <Switch>
+        <Route path="/" component={LoginPage} exact={true} />
+        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+        <PrivateRoute path="/create" component={AddExpensePage} />
+        <PrivateRoute path="/edit/:id" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </Router>
+);
 
-
-        
-  
-
-    const AppRouter=()=>(
-        <BrowserRouter>
-        <div>
-        <Header/>
-        <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage}/>
-        <Route  component={NotFoundPage} />
-        </Switch>
-        </div>
-        
-        </BrowserRouter>
-
-    );    
-   
-    export default AppRouter;
+export default AppRouter;
